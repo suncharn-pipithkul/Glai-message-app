da4bed7ddd392abf39862510af44255ab7866431
Thomas <suncharn.pipithkul@gmail.com>
2021-08-26 00:27:32 -0400

    add clickable sign up text & button styles

--- screens/welcome.js
+++ screens/welcome.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { Text, View, Button, TextInput } from 'react-native';
-import { Input } from 'react-native-elements';
+import React, { useState } from 'react';
+import { Text, View, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { Input, Button } from 'react-native-elements';
 import { Feather } from '@expo/vector-icons';
 import { globalStyles } from '../styles/globalStyles';
 
@@ -9,40 +9,41 @@ export default function WelcomeScreen() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    useEffect(() => {
-        console.log('email: ' + email);
-        console.log('password: ' + password);
-    }, [email, password])
-
     return (
-        <View style={globalStyles.container}>
-            <Text>Welcome!</Text>
-            <Text>Sign In</Text>
-            
-            <Input
-                leftIcon={<Feather name="mail" size={24} color="black" />}
-                label='Email'
-                placeholder='Enter your Email'
-                value={email}
-                onChangeText={ val => {
-                    setEmail(val);
-                    console.log(email);
-                }}
-                />
-            <Input
-                leftIcon={<Feather name="lock" size={24} color="black" />}
-                secureTextEntry
-                label='Password'
-                placeholder='Enter your password'
-                value={password}
-                onChangeText={ val => {
-                    setPassword(val);
-                    console.log(password);
-                }}
-                />
+        <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <View style={globalStyles.container}>
+                <Text style={globalStyles.headerText}>Welcome!</Text>
+                <Text style={globalStyles.headerText}>Sign In</Text>
+                
+                <Input
+                    leftIcon={<Feather name="mail" size={24} color="black" />}
+                    label='Email'
+                    placeholder='Enter your Email'
+                    onChangeText={ text => setEmail(text)}
+                    />
+                <Input
+                    leftIcon={<Feather name="lock" size={24} color="black" />}
+                    secureTextEntry
+                    label='Password'
+                    placeholder='Enter your password'
+                    onChangeText={ text => setPassword(text)}
+                    />
 
-            <Button title='LOGIN'></Button>
-            <Text>Don't have an Account? <Text style={{fontWeight: 'bold'}}>Sign up</Text></Text>
-        </View>
+                <Button 
+                    containerStyle={globalStyles.button} 
+                    buttonStyle={globalStyles.button}
+                    title='LOGIN'
+                    raised
+                    onPress={() => {
+                        console.log(`email=${email} | pass=${password}`);
+                        Keyboard.dismiss();
+                    }}
+                    />
+                    
+                <Text>Don't have an Account?
+                    <Text style={{fontWeight:'bold'}} onPress={() => console.log('sign up')}>Sign up</Text>
+                </Text>
+            </View>
+        </TouchableWithoutFeedback>
     );
 }
--- styles/globalStyles.js
+++ styles/globalStyles.js
@@ -2,9 +2,18 @@ import { StyleSheet } from 'react-native';
 
 export const globalStyles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+        margin: 32,
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    headerText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    button: {
+        width: '100%',
+        borderRadius: 30,
     },
   });
