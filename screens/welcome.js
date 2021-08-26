af092ce9f3907a721bbcbc1af1d3ffcb96f76ebb
Thomas <suncharn.pipithkul@gmail.com>
2021-08-25 22:33:22 -0400

    add icons in welcome screen

--- screens/welcome.js
+++ screens/welcome.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import { Text, View, Button, TextInput } from 'react-native';
 import { Input } from 'react-native-elements';
+import { Feather } from '@expo/vector-icons';
 import { globalStyles } from '../styles/globalStyles';
+
 
 export default function WelcomeScreen() {
     return (
         <View style={globalStyles.container}>
             <Text>Welcome!</Text>
             <Text>Sign In</Text>
+            
             <Input
+                leftIcon={<Feather name="mail" size={24} color="black" />}
                 label='Email'
                 mode='outlined'
                 placeholder='Enter your Email'/>
             <Input
+                leftIcon={<Feather name="lock" size={24} color="black" />}
                 secureTextEntry
                 label='Password'
                 placeholder='Enter your password'/>
+                
             <Button title='LOGIN'></Button>
             <Text>Don't have an Account? <Text style={{fontWeight: 'bold'}}>Sign up</Text></Text>
         </View>
