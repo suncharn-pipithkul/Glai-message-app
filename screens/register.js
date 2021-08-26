d176ab066c0d57e68162799e7ab8e2470af26275
Manuelo <57776467+ManueloTanjay@users.noreply.github.com>
2021-08-25 22:06:47 -0400

    added basic layout for registration page

--- screens/register.js
+++ screens/register.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Button, Text, TextInput, View } from 'react-native';
 import { globalStyles } from '../styles/globalStyles';
 
 
 export default function RegisterScreen(){
     return (
-        <View>
+        <View style={globalStyles.container}>
             <Text>Register screen</Text>
+            <Text>Username</Text>
+            <TextInput placeholder='Enter username'/>
+            <Text>Email</Text>
+            <TextInput placeholder='Enter email address'/>
+            <Text>password</Text>
+            <TextInput secureTextEntry='true' placeholder='password'/>
+            <Button title='REGISTER'></Button>
         </View>
     );
 }
