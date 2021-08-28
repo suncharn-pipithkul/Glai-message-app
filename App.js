1bc7eed0116a8acda6268fea4bfffe6fb5f1b6af
Thomas <suncharn.pipithkul@gmail.com>
2021-08-28 18:03:09 -0400

    attempt to make onBoarding screen shows only on first app launch using AsyncStorage

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 // @refresh reset
 // import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View } from 'react-native';
 import { globalStyles } from './styles/globalStyles';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
@@ -18,13 +19,38 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   const [isFirstLaunch, setIsFirstLaunch] = useState(true);
 
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown:false}}>
-        <Stack.Screen name='Onboard' component={OnBoardScreen} />
-        <Stack.Screen name='Login' component={LoginScreen} />
-        <Stack.Screen name='Register' component={RegisterScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  useEffect(() => {
+    AsyncStorage.getItem('@isFirstLaunch')
+      .then(val => {
+        if (val) {
+          AsyncStorage.setItem('@isFirstLaunch', 'true');
+        } else {
+          AsyncStorage.setItem('@isFirstLaunch', 'false');
+          setIsFirstLaunch(false);
+        }
+      });
+  }, []);
+
+  if (isFirstLaunch) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown:false}}>
+          {isFirstLaunch && <Stack.Screen name='Onboard' component={OnBoardScreen} />}
+          <Stack.Screen name='Login' component={LoginScreen} />
+          <Stack.Screen name='Register' component={RegisterScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+  else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown:false}}>
+          <Stack.Screen name='Login' component={LoginScreen} />
+          <Stack.Screen name='Register' component={RegisterScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+  
 }
