91a2c08bc523bc527790daa7ad5a068d10e08963
Thomas <suncharn.pipithkul@gmail.com>
2021-09-04 19:27:35 -0400

    change node_modules\react-native-gifted-chat\lib\MessageContainer.js because removeListener() in official gifted chat is deprecated in RN 0.65 and broke gifted chat completely. See more here: https://github.com/FaridSafi/react-native-gifted-chat/issues/2090

--- screens/chat.js
+++ screens/chat.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useEffect, useCallback, useContext } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-elements';
 import { GiftedChat } from 'react-native-gifted-chat'
@@ -14,17 +14,43 @@ export default function ChatScreen({ navigation }) {
     const { user, onSignout } = useContext(AuthContext);
     const [messages, setMessages] = useState([]);
 
+    useEffect(() => {
+        setMessages([
+          {
+            _id: 1,
+            text: 'Hello developer',
+            createdAt: new Date(),
+            user: {
+              _id: 2,
+              name: 'React Native',
+              avatar: 'https://placeimg.com/140/140/any',
+            },
+          },
+        ])
+      }, [])
+    
+      const onSend = useCallback((messages = []) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+      }, [])
+
     return (
-        <View>
-            <Text>Chat Screen</Text>
-            <Button 
-                title='Return'
-                containerStyle={globalStyles.button} 
-                buttonStyle={globalStyles.button}
-                raised
-                onPress={() => navigation.navigate('Main')}
-            />
-        </View>
+        <GiftedChat
+        messages={messages}
+        onSend={messages => onSend(messages)}
+        user={{
+            _id: 1,
+        }}
+        />
+        // <View>
+        //     <Text>Chat Screen</Text>
+        //     <Button 
+        //         title='Return'
+        //         containerStyle={globalStyles.button} 
+        //         buttonStyle={globalStyles.button}
+        //         raised
+        //         onPress={() => navigation.navigate('Main')}
+        //     />
+        // </View>
     );
 }
 
