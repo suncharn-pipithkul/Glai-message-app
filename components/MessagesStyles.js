bcb71edf8f5d98f8adfd09976ae2bd529c96f6c7
Thomas <suncharn.pipithkul@gmail.com>
2021-09-03 21:31:40 -0400

    Try a new main chat room layout

--- components/MessagesStyles.js
+++ components/MessagesStyles.js
@@ -7,11 +7,12 @@ export const Container = styled.View`
   /* padding-right: 20px; */
   align-items: center;
   background-color: #ffffff;
+  box-shadow: 10px 5px 5px black;
 `;
 
 export const Card = styled.TouchableOpacity`
     width: 100%;
-    /* background-color: #ddd; */
+    /* background-color: red; */
     /* margin-top: 5px; */
     border-radius: 15px;
 `;
@@ -19,37 +20,78 @@ export const Card = styled.TouchableOpacity`
 export const UserInfo = styled.View`
     flex-direction: row;
     /* background-color: skyblue; */
+    width: 100%;
+    border-radius: 15px;
     /* justify-content: space-between; */
 `;
 
 export const UserImgWrapper = styled.View`
-    padding-top: 15px;
-    padding-left: 15px;
-    padding-bottom: 15px;
+    padding-top: 10px;
+    padding-left: 10px;
+    padding-bottom: 10px;
     align-self: center;
-    /* background-color: red; */
+    background-color: crimson;
+    border-radius: 15px;
+    /* width: 20%; */
 `;
 
 export const UserImg = styled.Image`
-    width: 50px;
-    height: 50px;
-    border-radius: 25px;
+    width: 55px;
+    height: 55px;
+    border-radius: 35px;
 `;
 
-export const TextWrapper = styled.View`
+export const MiddleTextWrapper = styled.View`
     /* background-color: antiquewhite; */
-    width: 75%;
+    border-radius: 10px;
+    width: 60%;
     flex-direction: column;
     justify-content: center;
     padding: 15px;
 `;
 
 export const UserName = styled.Text`
-    font-size: 14px;
+    font-size: 18px;
     font-weight: bold;
+    color: #444444;
 `;
 
 export const MessageText = styled.Text`
     font-size: 14px;
-    color: #333333;
+    color: #525252;
+`;
+
+export const EndTextWrapper = styled.View`
+    /* background-color: yellowgreen; */
+    border-radius: 5px;
+    padding: 15px;
+    align-self: flex-end;
+
+`;
+
+export const SendAtText = styled.Text`
+    font-size: 14px;
+    color: #525252;
+`;
+
+export const MainTextWrapper = styled.View`
+    background-color: peru;
+    border-radius: 5px;
+    /* width: 80%; */
+    justify-content: center;
+    align-self: stretch;
+`;
+
+export const TopTextWrapper = styled.View`
+    background-color: yellowgreen;
+    border-radius: 5px;
+    padding-right: 10px;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const BottomTextWrapper = styled.View`
+    background-color: antiquewhite;
+    border-radius: 5px;
+    margin-top: 3px;
 `;
--- screens/main.js
+++ screens/main.js
@@ -1,7 +1,9 @@
 import React, { useState, useContext, useCallback } from 'react';
 import { StyleSheet, Text, View, FlatList, TouchableHighlight } from 'react-native';
 import { Button } from 'react-native-elements';
-import { Container, Card, TextWrapper, UserImg, UserImgWrapper, UserInfo, UserName, MessageText } from '../components/MessagesStyles';
+import { Container, Card, MiddleTextWrapper, UserImg
+    , UserImgWrapper, UserInfo, UserName, MessageText
+    , EndTextWrapper, SendAtText, MainTextWrapper, TopTextWrapper, BottomTextWrapper } from '../components/MessagesStyles';
 
 // Context
 import { AuthContext } from '../context/AuthContext';
@@ -15,69 +17,69 @@ const example = [
         userImg: require('../assets/profileImg/user-0.jpg'),
         user: 'Bill',
         recentMessage: 'Internal state is not preserved when content scrolls out of the render window. Make sure all your data is captured in the item data or external stores like Flux, Redux, or Relay.',
-        sendAt: '1:20 PM'
+        sendAt: '1:20 pm'
     },
     {
         id: '2',
         userImg: require('../assets/profileImg/user-1.jpg'),
         user: 'Thomas',
         recentMessage: 'By default, the list looks for a key prop on each item and uses that for the React key. Alternatively, you can provide a custom keyExtractor prop.',
-        sendAt: '12:25 PM'
+        sendAt: '12:25 pm'
     },
     {
         id: '3',
         userImg: require('../assets/profileImg/user-2.jpg'),
-        user: 'Boogi',
+        user: 'Boo',
         recentMessage: 'Takes an item from data and renders it into the list.',
-        sendAt: '7:20 AM'
+        sendAt: '7:20 am'
     },
     {
         id: '4',
         userImg: require('../assets/profileImg/user-3.jpg'),
-        user: 'Jugi',
+        user: 'Hammy',
         recentMessage: 'Meow meow.',
-        sendAt: '1:20 AM'
+        sendAt: '1:20 am'
     },
     {
         id: '5',
         userImg: require('../assets/profileImg/user-4.jpg'),
-        user: 'Jugi',
+        user: 'Hammy',
         recentMessage: 'Meow meow.',
-        sendAt: '1:20 AM'
+        sendAt: '1:20 am'
     },
     {
         id: '6',
         userImg: require('../assets/profileImg/user-5.jpg'),
-        user: 'Jugi',
+        user: 'Hammy',
         recentMessage: 'Meow meow.',
-        sendAt: '1:20 AM'
+        sendAt: '1:20 am'
     },
     {
         id: '7',
         userImg: require('../assets/profileImg/user-6.jpg'),
-        user: 'Jugi',
+        user: 'Hammy',
         recentMessage: 'Meow meow.',
-        sendAt: '1:20 AM'
+        sendAt: '1:20 am'
     },
     {
         id: '8',
         userImg: require('../assets/profileImg/user-7.jpg'),
-        user: 'Jugi',
+        user: 'Hammy',
         recentMessage: 'Meow meow.',
-        sendAt: '1:20 AM'
+        sendAt: '1:20 am'
     },
     {
         id: '9',
         userImg: require('../assets/profileImg/user-8.jpg'),
-        user: 'Jugi',
+        user: 'Hammy',
         recentMessage: 'Meow meow.',
-        sendAt: '1:20 AM'
+        sendAt: '1:20 am'
     },
 ];
 
 // a function that cut off the text longer than MAX_LENGTH and put ...
 const textDisplayFormat = (text) => {
-    const MAX_LENGTH = 35;
+    const MAX_LENGTH = 30;
     if (text.trim().length < MAX_LENGTH)
         return text.trim()
     return `${text.trim().substr(0, MAX_LENGTH)}...`
@@ -98,19 +100,26 @@ export default function MainScreen({ navigation }) {
                 style={{width:'100%'}}
                 data={example}
                 keyExtractor={item => item.id}
-                showsVerticalScrollIndicator={false}
+                // showsVerticalScrollIndicator={true}
                 refreshing={refreshing}
                 onRefresh={onRefresh}
                 renderItem={({ item }) => (
-                    <Card activeOpacity={0.5}>
+                    <Card activeOpacity={0.5} onPress={() => alert(item.user)}>
                         <UserInfo>
                             <UserImgWrapper>
                                 <UserImg source={item.userImg}/>
                             </UserImgWrapper>
-                            <TextWrapper>
-                                <UserName>{textDisplayFormat(item.user)}</UserName>
-                                <MessageText>{textDisplayFormat(item.recentMessage)}</MessageText>
-                            </TextWrapper>
+
+                            <MainTextWrapper>
+                                <TopTextWrapper>
+                                    <UserName>{textDisplayFormat(item.user)}</UserName>
+                                    <SendAtText>{item.sendAt}</SendAtText>
+                                </TopTextWrapper>
+
+                                <BottomTextWrapper>
+                                    <MessageText>{textDisplayFormat(item.recentMessage)}</MessageText>
+                                </BottomTextWrapper>
+                            </MainTextWrapper>
                         </UserInfo>
                     </Card>
                 )}
