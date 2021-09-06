a6ac22e9fd0dafcd603cd471aba3ceb5b419b6e7
Thomas <suncharn.pipithkul@gmail.com>
2021-09-05 23:56:25 -0400

    add safeAreaProvider

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { StatusBar, StyleSheet} from 'react-native';
 // Context
 import { AuthProvider } from './context/AuthContext';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 // Navigation
 import MainNavigation from './Navigations/MainNavigation';
@@ -10,10 +11,12 @@ import MainNavigation from './Navigations/MainNavigation';
 
 export default function App() {
   return (
-    <AuthProvider>
-      {/* <StatusBar barStyle = 'dark-content'/> */}
-      <MainNavigation/>
-    </AuthProvider>
+    <SafeAreaProvider>
+      <AuthProvider>
+        {/* <StatusBar barStyle = 'dark-content'/> */}
+        <MainNavigation/>
+      </AuthProvider>
+    </SafeAreaProvider>
   );
 }
 
